897566bceedd34d2ced2e134fb85f06f2e8dd449
Sam Deane <sam@elegantchaos.com>
2022-07-18 14:43:13 +0100

    Added --edit-config option.

--- Sources/ActionBuilderTool/ActionBuilderTool.swift
+++ Sources/ActionBuilderTool/ActionBuilderTool.swift
@@ -4,30 +4,39 @@
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 import ActionBuilderCore
-import AppKit
 import Foundation
+
+#if canImport(AppKit)
+import AppKit
+#endif
 
 @main struct ActionBuilderTool {
     static func main() throws {
-        let args = ProcessInfo.processInfo.arguments
-        guard args.count >= 2 else {
-            fatalError("Usage: \(args[0]) <package>")
+        let all = ProcessInfo.processInfo.arguments
+        let args = all.filter({ !$0.starts(with: "--") })
+        guard args.count == 2 else {
+            fatalError("Usage: \(args[0]) <options> <package>")
         }
         
         let url = URL(fileURLWithPath: args[1])
         let repo = try Repo(forPackage: url)
         
-        if shouldMakeSettings(arguments: args) {
+        let options = Set(all.filter({ $0.starts(with: "--") }))
+        if options.contains("--create-config") {
             makeSettings(for: repo, at: url)
         }
         
-        if shouldRevealSettings(arguments: args) {
+        if options.contains("--reveal-config") {
             revealSettings(for: repo, at: url)
+        }
+        
+        if options.contains("--edit-config") {
+            editSettings(for: repo, at: url)
         }
         
         let generator = Generator(name: "ActionBuilderTool", version: "1.0", link: "https://github.com/elegantchaos/ActionBuilderCore")
         try updateWorkflow(for: repo, at: url, with: generator)
-
+        
         if repo.header {
             try updateHeader(for: repo, at: url, with: generator)
         }
@@ -56,10 +65,6 @@ import Foundation
         try data?.write(to: readmeURL)
     }
     
-    static func shouldMakeSettings(arguments: [String]) -> Bool {
-        return ProcessInfo.processInfo.arguments.contains("--create-config")
-    }
-    
     static func makeSettings(for repo: Repo, at url: URL) {
         let settingsURL = Repo.settingsURL(forPackage: url)
         if !FileManager.default.fileExists(atPath: settingsURL.path) {
@@ -73,14 +78,19 @@ import Foundation
             }
         }
     }
-
-    static func shouldRevealSettings(arguments: [String]) -> Bool {
-        return ProcessInfo.processInfo.arguments.contains("--reveal-config")
-    }
     
     static func revealSettings(for repo: Repo, at url: URL) {
         let settingsURL = Repo.settingsURL(forPackage: url)
-        print(settingsURL)
+#if canImport(AppKit)
         NSWorkspace.shared.selectFile(settingsURL.path, inFileViewerRootedAtPath: "")
+#endif
     }
+
+    static func editSettings(for repo: Repo, at url: URL) {
+        let settingsURL = Repo.settingsURL(forPackage: url)
+#if canImport(AppKit)
+        NSWorkspace.shared.open(settingsURL)
+#endif
+    }
+
 }
