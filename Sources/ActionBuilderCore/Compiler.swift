b51943db58d173ea6ebf320841d90fc7a500c2bf
Sam Deane <sam@elegantchaos.com>
2025-08-28 15:51:59 +0100

    Added 6.1/6.2 logger support

--- Sources/ActionBuilderCore/Compiler.swift
+++ Sources/ActionBuilderCore/Compiler.swift
@@ -51,11 +51,13 @@ public final class Compiler: Identifiable, Sendable {
     case swift58
     case swift59
     case swift60
+    case swift61
+    case swift62
 
     case swiftLatest
     case swiftNightly
 
-    static let latestRelease = Self.swift60
+    static let latestRelease = Self.swift62
   }
 
   public static let compilers: [Compiler] = [
@@ -65,44 +67,54 @@ public final class Compiler: Identifiable, Sendable {
     Compiler(
       .swift50, name: "Swift 5.0", short: "5.0", linux: "swift:5.0",
       mac: .xcode(version: "11.2.1", image: "macos-10.15")),
+
     Compiler(
       .swift51, name: "Swift 5.1", short: "5.1", linux: "swift:5.1",
       mac: .xcode(version: "11.3.1", image: "macos-10.15")),
+
     Compiler(
       .swift52, name: "Swift 5.2", short: "5.2", linux: "swift:5.2.3-bionic",
       mac: .xcode(version: "11.7", image: "macos-11")),
+
     Compiler(
       .swift53, name: "Swift 5.3", short: "5.3", linux: "swift:5.3.3-bionic",
       mac: .xcode(version: "12.4", image: "macos-11")),
+
     Compiler(
       .swift54, name: "Swift 5.4", short: "5.4", linux: "swift:5.4.2-bionic",
       mac: .xcode(version: "12.5.1", image: "macos-11")),
+
     Compiler(
       .swift55, name: "Swift 5.5", short: "5.5", linux: "swift:5.5.3-bionic",
       mac: .xcode(version: "13.0", image: "macos-11")),
+
     Compiler(
       .swift56, name: "Swift 5.6", short: "5.6", linux: "swift:5.6.2-bionic",
       mac: .xcode(version: "13.4.1", image: "macos-12")),
 
-    // https://download.swift.org/development/xcode/swift-5.7-DEVELOPMENT-SNAPSHOT-2022-06-26-a/swift-5.7-DEVELOPMENT-SNAPSHOT-2022-06-26-a-osx.pkg
     Compiler(
       .swift57, name: "Swift 5.7", short: "5.7", linux: "swiftlang/swift:nightly-5.7-bionic",
       mac: .toolchain(version: "13.4.1", branch: "swift-5.7-branch", image: "macos-12")),
 
-    // https://download.swift.org/development/xcode/swift-5.7-DEVELOPMENT-SNAPSHOT-2022-06-26-a/swift-5.7-DEVELOPMENT-SNAPSHOT-2022-06-26-a-osx.pkg
     Compiler(
       .swift58, name: "Swift 5.8", short: "5.8", linux: "swiftlang/swift:nightly-5.8-bionic",
       mac: .toolchain(version: "13.4.1", branch: "swift-5.8-branch", image: "macos-12")),
 
-    // https://download.swift.org/development/xcode/swift-5.7-DEVELOPMENT-SNAPSHOT-2022-06-26-a/swift-5.7-DEVELOPMENT-SNAPSHOT-2022-06-26-a-osx.pkg
     Compiler(
       .swift59, name: "Swift 5.9", short: "5.9", linux: "swiftlang/swift:nightly-5.9-bionic",
       mac: .toolchain(version: "13.4.1", branch: "swift-5.9-branch", image: "macos-12")),
 
-    // https://download.swift.org/development/xcode/swift-5.7-DEVELOPMENT-SNAPSHOT-2022-06-26-a/swift-5.7-DEVELOPMENT-SNAPSHOT-2022-06-26-a-osx.pkg
     Compiler(
       .swift60, name: "Swift 6.0", short: "6.0", linux: "ubuntu-22.04",
       mac: .xcode(version: "16.0.0", image: "macos-14")),
+
+    Compiler(
+      .swift61, name: "Swift 6.1", short: "6.1", linux: "ubuntu-22.04",
+      mac: .xcode(version: "16.3.0", image: "macos-15")),
+
+    Compiler(
+      .swift62, name: "Swift 6.2", short: "6.2", linux: "ubuntu-22.04",
+      mac: .xcode(version: "26.0.0", image: "macos-15")),
 
     // https://download.swift.org/development/xcode/swift-DEVELOPMENT-SNAPSHOT-2022-03-22-a/swift-DEVELOPMENT-SNAPSHOT-2022-03-22-a-osx.pkg
     Compiler(
